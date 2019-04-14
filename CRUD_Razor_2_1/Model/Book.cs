284a8e4e04bc65db12774b39fffba85a5081fadf
Yunfei Li <uryu.li3@gmail.com>
2019-04-14 03:01:41 -0400

    initial commit

---
+++ CRUD_Razor_2_1/Model/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Razor_2_1.Model
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
