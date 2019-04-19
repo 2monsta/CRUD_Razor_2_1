104f396ef2279e1b093214208e31e7712bf7b056
Yunfei Li <uryu.li3@gmail.com>
2019-04-18 23:19:43 -0400

    added edit and delete buttons

---
+++ CRUD_Razor_2_1/Migrations/20190419025118_ChangeBookPropNameBack.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD_Razor_2_1.Migrations
+{
+    public partial class ChangeBookPropNameBack : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NameOfBook",
+                table: "Books",
+                newName: "Name");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Books",
+                newName: "NameOfBook");
+        }
+    }
+}
--- CRUD_Razor_2_1/Model/Book.cs
+++ CRUD_Razor_2_1/Model/Book.cs
@@ -10,7 +10,7 @@ namespace CRUD_Razor_2_1.Model
     {
         public int ID { get; set; }
         [Required]
-        public string NameOfBook { get; set; }
+        public string Name { get; set; }
 
         public string ISBN { get; set; }
         public string Author { get; set; }
